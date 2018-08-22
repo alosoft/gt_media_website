73519e494cf3531f75494268a0cef25dd4685584
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-08-22 09:59:57 +0000

    logging 505 error on heroku

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.use(function (req, res) {
 // Handle 500
 app.use(function (error, req, res, next) {
     res.send('500: Internal Server Error', 500);
+    console.log(error);
 });
 
 // error handler
